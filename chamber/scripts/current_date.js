5c5b7169b82153e742d14b4fb6bb8d032f43d605
JohnShirley1 <taylorshirley7749@gmail.com>
2023-01-27 21:23:37 -0700

    update

---
+++ chamber/scripts/current_date.js
@@ -0,0 +1,16 @@
+
+const datefield = document.querySelector(".date");
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('.ham');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('responsive');
+})
