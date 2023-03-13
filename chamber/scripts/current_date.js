4b9db1ada35a325b60dba81e90ab2fb2af40db70
JohnShirley1 <taylorshirley7749@gmail.com>
2023-03-13 12:47:52 -0600

    up

--- chamber/scripts/current_date.js
+++ chamber/scripts/current_date.js
@@ -49,24 +49,40 @@ hambutton.addEventListener('click', () => {
     mainnav.classList.toggle('responsive');
 })
 
-/*-------------------------------------------------------- Dynamic Windchill -------------------------------------------------------------- */
 
-const temp = parseFloat(document.querySelector('.t').textContent);
-const windspeed = parseFloat(document.querySelector('.ws').textContent);
+/*------------------------------------------------------ weather API call -------------------------------------------------------------- */
 
+const apiURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/48316?unitGroup=metric&key=9V2RKJSJVEG7V7TE3NY9QTE38&contentType=json";
+const getWeather = async () => {
+    const response = await fetch(apiURL);
+    const data = await response.json();
+    console.log(data);
+    let t = data.currentConditions.temp;
+    document.querySelector('#temp').textContent = t;
+    let image = `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/SVG/1st%20Set%20-%20Color/${data.currentConditions.icon}.svg`;
+    document.querySelector('#speed').textContent = data.currentConditions.windspeed;
+    document.querySelector('#condition').textContent =  data.currentConditions.conditions;
+    document.querySelector('#weathericon').src = image;
+    document.querySelector('#weathericon').alt= data.currentConditions.conditions + ' icon';
 
-let windchill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
+/*-------------------------------------------------------Wind Chill Calculation---------------------------------------------------------------*/
 
-console.log(windchill);
-if (temp<= 50 && windspeed >= 3) {
-    document.querySelector('.wc').innerHTML = windchill + "&#176;F";
-}
-else {
-  document.querySelector('.wc').innerHTML = 'N/A';
-}
+    let notapplicapble = " N/A"
+    let temp = parseFloat(document.getElementById('temp').textContent);
+    let windspeed = parseFloat(document.getElementById('speed').textContent);
+    let windchill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
+
+    if (temp <= 50.00 && windspeed >= 3.00) {
+      document.getElementById('chill').innerHTML = " " + windchill + "&#176;F";
+    }
+    else{
+      document.getElementById('chill').innerHTML = notapplicapble;
+    };
+};
+getWeather();
 
 /*------------------------------------------------------Storage local ------------------------------------------------------------------------*/
 
 if (!localStorage.getItem('lastvist')) {
   localStorage.getItem('lastvisit', date.now())
-}
+};
